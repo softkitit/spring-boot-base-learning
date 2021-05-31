40ad2e0a8f0223573e05e6b0b812131af923892e
Vitalii Samofal <vsamofal@softkitit.com>
2021-05-31 09:10:21 +0300

    SK-00 updated postman collection name

--- src/main/java/com/softkit/model/User.java
+++ src/main/java/com/softkit/model/User.java
@@ -17,11 +17,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
     @Size(min = 4, max = 255, message = "Minimum username length: 4 characters")
     @Column(unique = true, nullable = false)
     private String username;
+    
     @Column(unique = true, nullable = false)
     private String email;
+    
     @Size(min = 8, message = "Minimum password length: 8 characters")
     private String password;
 
